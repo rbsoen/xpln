adee0d5c960a23c16c8a3f9fed163652c18c8938
R Breil Soenoto <rbsoenoto@gmail.com>
2024-05-16 09:26:38 +0700

    split tangle into its own function

--- cmd/tangle.go
+++ cmd/tangle.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"xpln/internal"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +12,15 @@ var tangleCmd = &cobra.Command{
 	Use:   "tangle",
 	Short: "Extract source code",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("not implemented yet")
+		if len(args) < 1 {
+			fmt.Println("must provide at least 1 file")
+			os.Exit(1)
+		}
+		err := internal.Tangle(args...)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	},
 }
 
---
+++ internal/tangle.go
@@ -0,0 +1,7 @@
+package internal
+
+import "fmt"
+
+func Tangle(filenames ...string) error {
+	return fmt.Errorf("not implemented yet")
+}
