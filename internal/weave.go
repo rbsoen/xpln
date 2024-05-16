b072addedc4b184f61699c95fa594f43f9617b40
R Breil Soenoto <rbsoenoto@gmail.com>
2024-05-16 09:17:23 +0700

    internal/weave: documentation

--- internal/weave.go
+++ internal/weave.go
@@ -12,40 +12,71 @@ import (
 	"github.com/gomarkdown/markdown/parser"
 )
 
+// To link to other code blocks, use the
+// @{Name of code block} syntax.
 var linkDetect = regexp.MustCompile(
 	`^(\s*)@{(.+)}\s*$`,
 )
 
+// Used for the "used by" indicators for
+// each code block
 var codeUseMap = make(map[string][]string)
 
-func (b ProseBlock) ToHTML() string {
-	p := parser.NewWithExtensions(parser.CommonExtensions | parser.AutoHeadingIDs)
-	doc := p.Parse([]byte(b.Content))
-	r := html.NewRenderer(
-		html.RendererOptions{Flags: html.CommonFlags},
-	)
+func Weave(filenames ...string) error {
+	lines := make([]string, 0)
 
-	return string(markdown.Render(doc, r))
-}
+	// 1. `cat` every text file together
+	// into one giant text document
+	for _, filename := range filenames {
+		file, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
 
-func makeIdentifier(id string) string {
-	result := ""
-	lower := strings.ToLower(id)
-	for i := 0; i < len(lower); i++ {
-		if ((lower[i] >= '0') && (lower[i] <= '9')) ||
-			((lower[i] >= 'a') && (lower[i] <= 'z')) {
-			result = result + string(lower[i])
-		} else {
-			result = result + "-"
+		s := bufio.NewScanner(file)
+		for s.Scan() {
+			lines = append(lines, s.Text())
 		}
 	}
-	return result
+
+	// 2. Transform the text document into blocks
+	// of prose and code
+	b, err := ToBlocks(lines)
+	if err != nil {
+		return err
+	}
+
+	// 3. Track where each code block is used
+	// in other code blocks
+	for _, v := range b {
+		switch t := v.(type) {
+		case CodeBlock:
+			t.traceUsages()
+		}
+	}
+
+	// 4. Render the results
+	for _, v := range b {
+		switch t := v.(type) {
+		default:
+			return fmt.Errorf("unexpected type %T", t)
+		case ProseBlock:
+			fmt.Println(t.ToHTML())
+		case CodeBlock:
+			fmt.Println(t.ToHTML())
+		}
+	}
+	return err
 }
 
 func (b CodeBlock) traceUsages() {
+	// Match on a per-line basis
 	k := strings.Split(b.Content, "\n")
+
 	for i := 0; i < len(k); i++ {
 		if match := linkDetect.FindStringSubmatch(k[i]); match != nil {
+			// Update the key of the *referenced block* to add *this block*
 			codeUseMap[match[2]] = append(codeUseMap[match[2]], b.Name)
 		}
 	}
@@ -60,6 +91,7 @@ func (b CodeBlock) ToHTML() string {
 	result = append(result, `</header>`)
 	result = append(result, `<pre><code class="language-`+b.Language+`">`)
 
+	// Make the resulting code display as-is in the HTML
 	s := strings.ReplaceAll(b.Content, `&`, `&amp;`)
 	s = strings.ReplaceAll(s, `<`, `&lt;`)
 	s = strings.ReplaceAll(s, `>`, `&gt;`)
@@ -73,6 +105,8 @@ func (b CodeBlock) ToHTML() string {
 	result = append(result, "</code></pre>")
 
 	result = append(result, `<footer class="codeblock-footer">`)
+	// If this code block is referenced in other code blocks, add
+	// a "Used by" footer.
 	if n := len(codeUseMap[b.Name]); n > 0 {
 		result = append(result, `<span>Used by </span><ul>`)
 		for i := 0; i < n; i++ {
@@ -87,43 +121,27 @@ func (b CodeBlock) ToHTML() string {
 	return strings.Join(result, "\n")
 }
 
-func Weave(filenames ...string) error {
-	lines := make([]string, 0)
-
-	for _, filename := range filenames {
-		file, err := os.Open(filename)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		s := bufio.NewScanner(file)
-		for s.Scan() {
-			lines = append(lines, s.Text())
+// Turn the string into an HTML-safe identifier
+func makeIdentifier(id string) string {
+	result := ""
+	lower := strings.ToLower(id)
+	for i := 0; i < len(lower); i++ {
+		if ((lower[i] >= '0') && (lower[i] <= '9')) ||
+			((lower[i] >= 'a') && (lower[i] <= 'z')) {
+			result = result + string(lower[i])
+		} else {
+			result = result + "-"
 		}
 	}
+	return result
+}
 
-	b, err := ToBlocks(lines)
-	if err != nil {
-		return err
-	}
-
-	for _, v := range b {
-		switch t := v.(type) {
-		case CodeBlock:
-			t.traceUsages()
-		}
-	}
-
-	for _, v := range b {
-		switch t := v.(type) {
-		default:
-			return fmt.Errorf("unexpected type %T", t)
-		case ProseBlock:
-			fmt.Println(t.ToHTML())
-		case CodeBlock:
-			fmt.Println(t.ToHTML())
-		}
-	}
-	return err
+// Converting a prose block to HTML is relatively simple
+func (b ProseBlock) ToHTML() string {
+	p := parser.NewWithExtensions(parser.CommonExtensions | parser.AutoHeadingIDs)
+	doc := p.Parse([]byte(b.Content))
+	r := html.NewRenderer(
+		html.RendererOptions{Flags: html.CommonFlags},
+	)
+	return string(markdown.Render(doc, r))
 }
